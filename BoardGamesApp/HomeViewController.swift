bb012eacbc8b4dce998ca1dad494c68ec17b95e1
Ola <aleksandra.plichta@poczta.fm>
2025-08-04 07:57:39 +0200

    REFACTOR: Corrected our HomeView, creted more layout constraints, scrollView and stackview for our upcoming events.

--- BoardGamesApp/HomeViewController.swift
+++ BoardGamesApp/HomeViewController.swift
@@ -14,14 +14,16 @@ class HomeViewController: UIViewController{
     @IBOutlet var upcomingTitle: UILabel!
     @IBOutlet var pastTitle: UILabel!
     
-    let cardsStackView = UIStackView()
+    let scrollView = UIScrollView()
+    let daysStackView = UIStackView()
     
     
     var events: [GameEvent] = [
         GameEvent(title: "Monopoly", currentPlayersCount: 3, maxPlayersCount: 4, time: "10:00 - 12:00", location: "Planty Racławickie", date: Calendar.current.date(from: DateComponents(year: 2025, month: 8, day: 2))!),
         GameEvent(title: "Catan", currentPlayersCount: 2, maxPlayersCount: 4, time: "12:00 - 14:00", location: "Planty Racławickie", date: Calendar.current.date(from: DateComponents(year: 2025, month: 10, day: 12))!)
-                  
         ]
+    var groupedEvents = [Date: [GameEvent]]()
+    var sortedDays = [Date]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -29,33 +31,50 @@ class HomeViewController: UIViewController{
         upcomingTitle.text = "Nadchodzące spotkania"
         pastTitle.text = "Zakończone spotkania"
 
-        
-        setupCardsStackView()
-        displayGameCards()
+        groupEventsByDate()
+        setupScrollViewAndStackView()
+        displayTimeLine()
         
 
     }
     
-    func setupCardsStackView() {
-        view.addSubview(cardsStackView)
+    func groupEventsByDate(){
+        groupedEvents = Dictionary(grouping: events) { event in
+            return Calendar.current.startOfDay(for: event.date)
+        }
+        sortedDays = groupedEvents.keys.sorted()
         
-        cardsStackView.axis = .vertical
-        cardsStackView.spacing = 8
-        cardsStackView.translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+    func setupScrollViewAndStackView() {
+        view.addSubview(scrollView)
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
+        scrollView.backgroundColor = .blue
+        
+        daysStackView.axis = .vertical
+        daysStackView.spacing = 24
+        scrollView.addSubview(daysStackView)
+        daysStackView.translatesAutoresizingMaskIntoConstraints = false
+        
+        pastTitle.translatesAutoresizingMaskIntoConstraints = false
         
         NSLayoutConstraint.activate([
-            cardsStackView.topAnchor.constraint(equalTo: upcomingTitle.bottomAnchor, constant: 10),
-            cardsStackView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 70),
-            cardsStackView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -70)
+            scrollView.topAnchor.constraint(equalTo: upcomingTitle.bottomAnchor, constant: 8),
+            scrollView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            scrollView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            scrollView.heightAnchor.constraint(equalToConstant: 200),
+            pastTitle.topAnchor.constraint(equalTo: scrollView.bottomAnchor, constant: 8),
+            pastTitle.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 48),
+            daysStackView.topAnchor.constraint(equalTo: scrollView.contentLayoutGuide.topAnchor),
+            daysStackView.leadingAnchor.constraint(equalTo: scrollView.contentLayoutGuide.leadingAnchor),
+            daysStackView.trailingAnchor.constraint(equalTo: scrollView.contentLayoutGuide.trailingAnchor),
+            daysStackView.bottomAnchor.constraint(equalTo: scrollView.contentLayoutGuide.bottomAnchor),
+            daysStackView.widthAnchor.constraint(equalTo: scrollView.frameLayoutGuide.widthAnchor)
         ])
     }
     
-    func displayGameCards() {
-        for event in events {
-            let card = GameCardView()
-            card.configure(with: event)
-            cardsStackView.addArrangedSubview(card)
-        }
+    func displayTimeLine() {
+        
     }
 
 }
