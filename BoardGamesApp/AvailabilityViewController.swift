7db798feecf88a35b39462ba7fb3d83535008b5e
Ola <aleksandra.plichta@poczta.fm>
2025-08-06 08:46:28 +0200

    FEATURE: Add UI for adding availability.

---
+++ BoardGamesApp/AvailabilityViewController.swift
@@ -0,0 +1,49 @@
+//
+//  AvaiabilityViewController.swift
+//  BoardGamesApp
+//
+//  Created by Aleksandra Plichta on 05/08/2025.
+//
+
+import UIKit
+
+class AvailabilityViewController: UIViewController {
+    @IBOutlet var addAvailabilityContainer: UIView!
+    @IBOutlet var addAvailabilityTitle: UILabel!
+    @IBOutlet var dateLabel: UILabel!
+    @IBOutlet var chooseDateButton: UIButton!
+    @IBOutlet var durationLabel: UILabel!
+    @IBOutlet var durationFromButton: UIButton!
+    @IBOutlet var durationToButton: UIButton!
+    @IBOutlet var durationSummaryLabel: UILabel!
+    @IBOutlet var repetitionLabel: UILabel!
+    @IBOutlet var repetitionSwitch: UISwitch!
+    @IBOutlet var gamesLimitLabel: UILabel!
+    @IBOutlet var gamesLimitTextField: UITextField!
+    @IBOutlet var gamesLimitButton: UIButton!
+    @IBOutlet var addAvailabilityButton: UIButton!
+    @IBAction func didTapChooseDateButton(_ sender: Any) {
+    }
+    
+    @IBAction func didTapDurationFromButton(_ sender: Any) {
+    }
+    
+    @IBAction func didTapDurationToButton(_ sender: Any) {
+    }
+    
+    @IBAction func didChangedRepetitionSwitch(_ sender: UISwitch) {
+    }
+    
+    @IBAction func didTapGamesLimitButton(_ sender: Any) {
+    }
+    
+    @IBAction func didTapAddAvailabilityButton(_ sender: Any) {
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
