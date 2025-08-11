95a35d76fc85ce9883898e56133071a8933e82c2
Ola <aleksandra.plichta@poczta.fm>
2025-08-11 20:06:43 +0200

    REFACTOR & FEATURE: Refactor part: Refactor groupEventsByDate to function groupAndSort which is universal for different Events lists. Feature part: Add new  list of events created to store past, already finished events. Creat a stack and scroll view for this events and show them on screen.

--- BoardGamesApp/HomeViewController.swift
+++ BoardGamesApp/HomeViewController.swift
@@ -9,16 +9,16 @@ import UIKit
 
 class HomeViewController: UIViewController{
     @IBOutlet var greeting: UILabel!
-    @IBOutlet var addAviability: UIButton!
-    @IBOutlet var searchMeetings: UIButton!
     @IBOutlet var upcomingTitle: UILabel!
     @IBOutlet var pastTitle: UILabel!
     
     let scrollView = UIScrollView()
     let daysStackView = UIStackView()
+    let scrollViewPastEvents = UIScrollView()
+    let stackViewPastDays = UIStackView()
     
     
-    var events: [GameEvent] = [
+    var upcomingEvents: [GameEvent] = [
         GameEvent(title: "Monopoly", currentPlayersCount: 3, maxPlayersCount: 4, time: "10:00 - 12:00", location: "Planty Racławickie", date: Calendar.current.date(from: DateComponents(year: 2025, month: 8, day: 2))!),
         GameEvent(title: "Catan", currentPlayersCount: 2, maxPlayersCount: 4, time: "12:00 - 14:00", location: "Planty Racławickie", date: Calendar.current.date(from: DateComponents(year: 2025, month: 10, day: 12))!),
         GameEvent(title: "Catan", currentPlayersCount: 2, maxPlayersCount: 4, time: "12:00 - 14:00", location: "Planty Racławickie", date: Calendar.current.date(from: DateComponents(year: 2025, month: 10, day: 12))!),
@@ -26,8 +26,12 @@ class HomeViewController: UIViewController{
         GameEvent(title: "Catan", currentPlayersCount: 2, maxPlayersCount: 4, time: "12:00 - 14:00", location: "Planty Racławickie", date: Calendar.current.date(from: DateComponents(year: 2025, month: 10, day: 13))!),
         GameEvent(title: "Catan", currentPlayersCount: 2, maxPlayersCount: 4, time: "12:00 - 14:00", location: "Planty Racławickie", date: Calendar.current.date(from: DateComponents(year: 2025, month: 10, day: 13))!)
         ]
-    var groupedEvents = [Date: [GameEvent]]()
-    var sortedDays = [Date]()
+    
+    var pastEvents: [GameEvent] = [
+        GameEvent(title: "Endgame", currentPlayersCount: 5, maxPlayersCount: 6, time: "10:00 - 12:00", location: "Planty Racławickie", date: Calendar.current.date(from: DateComponents(year: 2025, month: 3, day: 2))!),
+        GameEvent(title: "NeedForSpeed", currentPlayersCount: 2, maxPlayersCount: 4, time: "12:00 - 14:00", location: "Planty Racławickie", date: Calendar.current.date(from: DateComponents(year: 2025, month: 3, day: 10))!),
+        GameEvent(title: "CitySkylines", currentPlayersCount: 2, maxPlayersCount: 4, time: "12:00 - 14:00", location: "Planty Racławickie", date: Calendar.current.date(from: DateComponents(year: 2025, month: 2, day: 15))!)
+    ]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -38,25 +42,37 @@ class HomeViewController: UIViewController{
         upcomingTitle.font = UIFont.systemFont(ofSize: 16, weight: .medium)
         pastTitle.font = UIFont.systemFont(ofSize: 16, weight: .medium)
 
-        groupEventsByDate()
-        setupScrollViewAndStackView()
-        displayTimeLine()
         
+        setUpcomingEvents()
+        setUpPastEvents()
 
     }
     
-    func groupEventsByDate(){
-        groupedEvents = Dictionary(grouping: events) { event in
+    func groupAndSort(events: [GameEvent]) -> (sortedDays: [Date], groupedEvents: [Date: [GameEvent]]){
+        let grouped = Dictionary(grouping: events) { event in
             return Calendar.current.startOfDay(for: event.date)
         }
-        sortedDays = groupedEvents.keys.sorted()
+        let sorted = grouped.keys.sorted()
+        return (sorted, grouped)
+        
+    }
+    
+    func setUpcomingEvents()  {
+        let upcomingData = groupAndSort(events: upcomingEvents)
+        setupScrollViewAndStackView()
+        displayTimeLine(for: upcomingData.sortedDays, with: upcomingData.groupedEvents, to: daysStackView)
+    }
+    
+    func setUpPastEvents() {
+        let pastData = groupAndSort(events: pastEvents)
+        setUpPastScrollViewAndStackView()
+        displayTimeLine(for: pastData.sortedDays, with: pastData.groupedEvents, to: stackViewPastDays)
         
     }
     
     func setupScrollViewAndStackView() {
         view.addSubview(scrollView)
         scrollView.translatesAutoresizingMaskIntoConstraints = false
-     //   scrollView.backgroundColor = .blue
         
         daysStackView.axis = .vertical
         daysStackView.spacing = 24
@@ -66,11 +82,11 @@ class HomeViewController: UIViewController{
         pastTitle.translatesAutoresizingMaskIntoConstraints = false
         
         NSLayoutConstraint.activate([
-            scrollView.topAnchor.constraint(equalTo: upcomingTitle.bottomAnchor, constant: 8),
+            scrollView.topAnchor.constraint(equalTo: upcomingTitle.bottomAnchor, constant: 16),
             scrollView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
             scrollView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-            scrollView.heightAnchor.constraint(equalToConstant: 200),
-            pastTitle.topAnchor.constraint(equalTo: scrollView.bottomAnchor, constant: 8),
+            scrollView.heightAnchor.constraint(equalToConstant: 250),
+            pastTitle.topAnchor.constraint(equalTo: scrollView.bottomAnchor, constant: 16),
             pastTitle.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 48),
             daysStackView.topAnchor.constraint(equalTo: scrollView.contentLayoutGuide.topAnchor),
             daysStackView.leadingAnchor.constraint(equalTo: scrollView.contentLayoutGuide.leadingAnchor),
@@ -80,13 +96,37 @@ class HomeViewController: UIViewController{
         ])
     }
     
-    func displayTimeLine() {
+    func setUpPastScrollViewAndStackView() {
+        view.addSubview(scrollViewPastEvents)
+        scrollViewPastEvents.translatesAutoresizingMaskIntoConstraints = false
+        
+        stackViewPastDays.axis = .vertical
+        stackViewPastDays.spacing = 24
+        scrollViewPastEvents.addSubview(stackViewPastDays)
+        stackViewPastDays.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            scrollViewPastEvents.topAnchor.constraint(equalTo: pastTitle.bottomAnchor, constant: 16),
+            scrollViewPastEvents.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            scrollViewPastEvents.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            scrollViewPastEvents.heightAnchor.constraint(equalToConstant: 250),
+            
+            stackViewPastDays.topAnchor.constraint(equalTo: scrollViewPastEvents.contentLayoutGuide.topAnchor),
+            stackViewPastDays.leadingAnchor.constraint(equalTo: scrollViewPastEvents.contentLayoutGuide.leadingAnchor),
+            stackViewPastDays.trailingAnchor.constraint(equalTo: scrollViewPastEvents.contentLayoutGuide.trailingAnchor),
+            stackViewPastDays.bottomAnchor.constraint(equalTo: scrollViewPastEvents.contentLayoutGuide.bottomAnchor),
+            stackViewPastDays.widthAnchor.constraint(equalTo: scrollViewPastEvents.frameLayoutGuide.widthAnchor)
+        ])
+        
+    }
+    
+    func displayTimeLine(for sortedDays: [Date], with groupedEvents: [Date: [GameEvent]],to stackView: UIStackView) {
         for day in sortedDays {
             guard let eventsForDay = groupedEvents[day] else {continue}
             
             let dayRow = DayTimeLineRowView()
             dayRow.configure(day, and: eventsForDay)
-            daysStackView.addArrangedSubview(dayRow)
+            stackView.addArrangedSubview(dayRow)
         }
     }
 
